a25b977ee960f05f3d1c773a560f238d92a11190
Emiliano M <19cg0130@gmail.com>
2024-08-26 15:08:38 -0600

    Ahora solo los usuarios que hayan iniciado sesión, pueden registrar nuevos usuarios

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,7 +45,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Iniciar sesión automáticamente tras el registro, no usar
+        //Auth::login($user);
 
         return redirect(route('dashboard', absolute: false));
     }
