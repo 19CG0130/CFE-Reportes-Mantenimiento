f6a34fbf7414d80afbd1c5ed00baf8631153744b
AlanMad24 <madridalansaulholguin@gmail.com>
2024-08-29 13:26:21 -0600

    Button nuevo registro

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
-        
+
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                        <div
-                            class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
-                            <div>
+                        <div class="flex flex-column sm:flex-row flex-wrap items-center justify-between pb-4">
+                            <div class="flex-1">
                                 <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio"
                                     class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                                     type="button">
@@ -27,66 +25,16 @@
                                             stroke-width="2" d="m1 1 4 4 4-4" />
                                     </svg>
                                 </button>
-                                
-                                <!-- Dropdown menu -->
-                                <div id="dropdownRadio"
-                                    class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-                                    data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top"
-                                    style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(522.5px, 3847.5px, 0px);">
-                                    <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
-                                        aria-labelledby="dropdownRadioButton">
-                                        <li>
-                                            <div
-                                                class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                                <input id="dia" type="radio" value=""
-                                                    name="filter-radio"
-                                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                                <label for="dia"
-                                                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">
-                                                    {{ __('Último día') }}
-                                                </label>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div
-                                                class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                                <input checked="" id="semana" type="radio"
-                                                    value="" name="filter-radio"
-                                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                                <label for="semana"
-                                                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">
-                                                    {{ __('Última semana ') }}
-                                                </label>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div
-                                                class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                                <input id="mes" type="radio" value=""
-                                                    name="filter-radio"
-                                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                                <label for="mes"
-                                                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">
-                                                    {{ __('Último mes') }}
-                                                </label>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div
-                                                class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                                <input id="todos" type="radio" value=""
-                                                    name="filter-radio"
-                                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                                <label for="todos"
-                                                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">
-                                                    {{ __('Todos') }}
-                                                </label>
-                                            </div>
-                                        </li>
-                                        
-                                    </ul>
-                                </div>
                             </div>
+
+                            <div class="flex-1  justify-center">
+                                <a href="{{ route('registro_mantenimiento') }}"
+                                    class="inline-flex items-center px-4 py-2 bg-green-600 dark:bg-green-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-green-900 uppercase tracking-widest hover:bg-green-500 dark:hover:bg-green-300 focus:bg-green-500 dark:focus:bg-green-300 active:bg-green-700 dark:active:bg-green-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 dark:focus:ring-offset-green-800 transition ease-in-out duration-150">
+                                    Nuevo Registro
+                                </a>
+                            </div>
+
+
                             <label for="table-search" class="sr-only">Search</label>
                             <div class="relative">
                                 <div
@@ -102,12 +50,14 @@
                                     class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Buscar por número de serie">
                             </div>
+
                         </div>
+
                         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             <thead
                                 class="text-xs text-gray-200 uppercase bg-gray-500 dark:bg-gray-700 dark:text-gray-400 rounded-lg">
                                 <tr class="divide-x divide-gray-500">
-                                    
+
                                     <th scope="col" class="px-6 py-3 text-center">
                                         Fecha
                                     </th>
@@ -115,12 +65,12 @@
                                         Número de Serie
                                     </th>
                                     <th scope="col" class="px-6 py-3 text-center">
-                                        RPE 
+                                        RPE
                                     </th>
                                     <th scope="col" class="px-6 py-3 text-center">
                                         Acciones
                                     </th>
-                                    
+
                                 </tr>
                             </thead>
                             <tbody class="divide-y divide-gray-500">
@@ -139,27 +89,54 @@
                                     <td class="px-6 py-4 flex justify-center space-x-2">
                                         <div class="flex justify-center space-x-2">
                                             <a href="#">
-                                                <svg class="text-gray-500 w-8 h-8"
-                                                xmlns="http://www.w3.org/2000/svg" width="24"  height="24"   viewBox="0 0 24 24"  stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />  <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />  <line x1="16" y1="5" x2="19" y2="8" /></svg>
+                                                <svg class="text-gray-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                    stroke="currentColor" fill="none" stroke-linecap="round"
+                                                    stroke-linejoin="round">
+                                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                                    <path
+                                                        d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+                                                    <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+                                                    <line x1="16" y1="5" x2="19" y2="8" />
+                                                </svg>
                                             </a>
                                             <a href="">
-                                                <svg class="text-green-500 w-8 h-8"
-                                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"/>
-                                                </svg>            
+                                                <svg class="text-green-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                                    width="24" height="24" fill="none" viewBox="0 0 24 24"
+                                                    stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
+                                                </svg>
                                             </a>
                                             <a href="">
-                                                <svg class="text-blue-500 w-8 h-8"
-                                                xmlns="http://www.w3.org/2000/svg" width="24"  height="24"   viewBox="0 0 20 20" fill="currentColor">  <path fill-rule="evenodd" d="M5 4v3H4a2 2 0 00-2 2v3a2 2 0 002 2h1v2a2 2 0 002 2h6a2 2 0 002-2v-2h1a2 2 0 002-2V9a2 2 0 00-2-2h-1V4a2 2 0 00-2-2H7a2 2 0 00-2 2zm8 0H7v3h6V4zm0 8H7v4h6v-4z" clip-rule="evenodd"/></svg>
+                                                <svg class="text-blue-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                                    width="24" height="24" viewBox="0 0 20 20"
+                                                    fill="currentColor">
+                                                    <path fill-rule="evenodd"
+                                                        d="M5 4v3H4a2 2 0 00-2 2v3a2 2 0 002 2h1v2a2 2 0 002 2h6a2 2 0 002-2v-2h1a2 2 0 002-2V9a2 2 0 00-2-2h-1V4a2 2 0 00-2-2H7a2 2 0 00-2 2zm8 0H7v3h6V4zm0 8H7v4h6v-4z"
+                                                        clip-rule="evenodd" />
+                                                </svg>
                                             </a>
                                             <a href="">
-                                                <svg class="text-red-500 w-8 h-8"
-                                                xmlns="http://www.w3.org/2000/svg" width="24"  height="24"   viewBox="0 0 24 24"  stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="4" y1="7" x2="20" y2="7" />  <line x1="10" y1="11" x2="10" y2="17" />  <line x1="14" y1="11" x2="14" y2="17" />  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
+                                                <svg class="text-red-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                                    width="24" height="24" viewBox="0 0 24 24"
+                                                    stroke-width="2" stroke="currentColor" fill="none"
+                                                    stroke-linecap="round" stroke-linejoin="round">
+                                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                                    <line x1="4" y1="7" x2="20"
+                                                        y2="7" />
+                                                    <line x1="10" y1="11" x2="10"
+                                                        y2="17" />
+                                                    <line x1="14" y1="11" x2="14"
+                                                        y2="17" />
+                                                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                                </svg>
                                             </a>
-                                    </div>
+                                        </div>
                                     </td>
                                 </tr>
-                                
+
                             </tbody>
                         </table>
                     </div>
