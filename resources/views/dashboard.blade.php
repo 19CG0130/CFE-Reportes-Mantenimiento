e944a49fd7de6f981b53caa1c8abc6ef0f308385
AlanMad24 <madridalansaulholguin@gmail.com>
2024-09-02 15:14:12 -0600

    print icon

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -219,8 +219,9 @@
                                                                 stroke="currentColor">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                                     stroke-width="2"
-                                                                    d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                                                    d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
                                                             </svg>
+
 
                                                             <div class="w-full text-lg font-semibold">Impresora
                                                             </div>
