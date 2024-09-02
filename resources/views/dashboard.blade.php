9dc38f028083f617e9338409521aad8f912e2d27
19cg0130 <19cg0130@gmail.com>
2024-09-02 12:58:04 -0600

    fix vista dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,13 +2,15 @@
     <div class="max-w-7xl py-6 mx-auto sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <div class="flex flex-column sm:flex-row flex-wrap items-center justify-between pb-4">
-                        <div class="flex-1">
+                <div class="relative">
+                    <!-- Seccion Superior -->
+                    <div class="flex flex-wrap items-center justify-between pb-4">
+                        <!-- Ordenar por fecha -->
+                        <div>
                             <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio"
                                 class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                                 type="button">
-                                <svg class="w-8 h-8 px-1 text-gray-500 dark:text-gray-400 me-3" aria-hidden="true"
+                                <svg class="w-6 h-6 px-1 text-gray-500 dark:text-gray-400 me-3" aria-hidden="true"
                                     xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                     <path
                                         d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
@@ -72,10 +74,8 @@
 
                             </ul>
                         </div>
-                        <!--MODAL SELECCIONAR TP,PC,TABLET-->
-                        <div class="flex-1  justify-center">
-
-
+                        <!--Boton Modal Seleccionar dispositivo-->
+                        <div>
                             <!-- Modal toggle -->
                             <button data-modal-target="select-modal" data-modal-toggle="select-modal"
                                 class="inline-flex items-center px-4 py-2 bg-green-600 dark:bg-green-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-green-900 uppercase tracking-widest hover:bg-green-500 dark:hover:bg-green-300 focus:bg-green-500 dark:focus:bg-green-300 active:bg-green-700 dark:active:bg-green-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 dark:focus:ring-offset-green-800 transition ease-in-out duration-150"
@@ -241,31 +241,30 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-                    <label for="table-search" class="sr-only">Search</label>
-                    <div class="relative">
-                        <div
-                            class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
+                        <!-- Buscar -->
+                        <label for="table-search" class="sr-only">Search</label>
+                        <div class="relative">
+                            <div
+                                class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                    fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </div>
+                            <input type="text" id="table-search"
+                                class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Buscar por número de serie">
                         </div>
-                        <input type="text" id="table-search"
-                            class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Buscar por número de serie">
                     </div>
-
                 </div>
 
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <!-- Tabla Registros -->
+                <table class="w-full shadow-md text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead
                         class="text-xs text-gray-200 uppercase bg-gray-500 dark:bg-gray-700 dark:text-gray-400 rounded-lg">
                         <tr class="divide-x divide-gray-500">
-
                             <th scope="col" class="px-6 py-3 text-center">
                                 Fecha
                             </th>
@@ -278,21 +277,143 @@
                             <th scope="col" class="px-6 py-3 text-center">
                                 Acciones
                             </th>
-
                         </tr>
                     </thead>
-                    <tbody class="divide-y divide-gray-500">
+
+                    <tbody class="divide-y divide-gray-200">
+                        <!-- Registro 1 -->
                         <tr
-                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th scope="row"
-                                class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                <label for=""></label>
-                            </th>
+                            class="bg-white dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                             <td class="px-6 py-4 text-center">
-                                Silver
+                                01/02/2024
                             </td>
                             <td class="px-6 py-4 text-center">
-                                Laptop
+                                OLK8FC2
+                            </td>
+                            <td class="px-6 py-4 text-center">
+                                CFE147
+                            </td>
+                            <td class="px-6 py-4 flex justify-center space-x-2">
+                                <div class="flex justify-center space-x-2">
+                                    <!--svg Editar registro-->
+                                    <a href="#" title="editar">
+                                        <svg class="text-gray-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                            stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" />
+                                            <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+                                            <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+                                            <line x1="16" y1="5" x2="19" y2="8" />
+                                        </svg>
+                                    </a>
+                                    <!--svg Ver registro-->
+                                    <a href="" title="ver">
+                                        <svg class="text-green-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                            width="24" height="24" fill="none" viewBox="0 0 24 24"
+                                            stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M4 6h16M4 10h16M4 14h16M4 18h16" />
+                                        </svg>
+                                    </a>
+                                    <!--svg Imprimir por PDF-->
+                                    <a href="" title="imprimir">
+                                        <svg class="text-blue-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                            width="24" height="24" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd"
+                                                d="M5 4v3H4a2 2 0 00-2 2v3a2 2 0 002 2h1v2a2 2 0 002 2h6a2 2 0 002-2v-2h1a2 2 0 002-2V9a2 2 0 00-2-2h-1V4a2 2 0 00-2-2H7a2 2 0 00-2 2zm8 0H7v3h6V4zm0 8H7v4h6v-4z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </a>
+                                    <!--svg Eliminar registro-->
+                                    <a href="" title="eliminar">
+                                        <svg class="text-red-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                            stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" />
+                                            <line x1="4" y1="7" x2="20" y2="7" />
+                                            <line x1="10" y1="11" x2="10" y2="17" />
+                                            <line x1="14" y1="11" x2="14" y2="17" />
+                                            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                        <!-- Registro 2 -->
+                        <tr
+                            class="bg-white dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="px-6 py-4 text-center">
+                                01/02/2024
+                            </td>
+                            <td class="px-6 py-4 text-center">
+                                FVF58D4
+                            </td>
+                            <td class="px-6 py-4 text-center">
+                                CFE258
+                            </td>
+                            <td class="px-6 py-4 flex justify-center space-x-2">
+                                <div class="flex justify-center space-x-2">
+                                    <!--svg Editar registro-->
+                                    <a href="#" title="editar">
+                                        <svg class="text-gray-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                            stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" />
+                                            <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+                                            <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+                                            <line x1="16" y1="5" x2="19" y2="8" />
+                                        </svg>
+                                    </a>
+                                    <!--svg Ver registro-->
+                                    <a href="" title="ver">
+                                        <svg class="text-green-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                            width="24" height="24" fill="none" viewBox="0 0 24 24"
+                                            stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M4 6h16M4 10h16M4 14h16M4 18h16" />
+                                        </svg>
+                                    </a>
+                                    <!--svg Imprimir por PDF-->
+                                    <a href="" title="imprimir">
+                                        <svg class="text-blue-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                            width="24" height="24" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd"
+                                                d="M5 4v3H4a2 2 0 00-2 2v3a2 2 0 002 2h1v2a2 2 0 002 2h6a2 2 0 002-2v-2h1a2 2 0 002-2V9a2 2 0 00-2-2h-1V4a2 2 0 00-2-2H7a2 2 0 00-2 2zm8 0H7v3h6V4zm0 8H7v4h6v-4z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </a>
+                                    <!--svg Eliminar registro-->
+                                    <a href="" title="eliminar">
+                                        <svg class="text-red-500 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                            stroke="currentColor" fill="none" stroke-linecap="round"
+                                            stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" />
+                                            <line x1="4" y1="7" x2="20" y2="7" />
+                                            <line x1="10" y1="11" x2="10" y2="17" />
+                                            <line x1="14" y1="11" x2="14" y2="17" />
+                                            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                        <!-- Registro 3 -->
+                        <tr
+                            class="bg-white dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="px-6 py-4 text-center">
+                                01/02/2024
+                            </td>
+                            <td class="px-6 py-4 text-center">
+                                FVF58D4
+                            </td>
+                            <td class="px-6 py-4 text-center">
+                                CFE258
                             </td>
                             <td class="px-6 py-4 flex justify-center space-x-2">
                                 <div class="flex justify-center space-x-2">
